8b98f1b21074cced76f8d99d2397f1a56721e7b1
SamRocan <SamClendenan@iCloud.com>
2021-06-04 11:02:30 +0100

    Updated GUI - GUI updated to closer match design - basic search bar implemented (non functional)

--- DissertationSite/DissertationSite/urls.py
+++ DissertationSite/DissertationSite/urls.py
@@ -24,7 +24,6 @@ from users import views as user_views
 
 urlpatterns = [
     path('', include('productParser.urls')),
-    # Maybe delete the lower one
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
